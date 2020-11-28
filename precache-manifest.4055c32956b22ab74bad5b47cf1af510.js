21b9e88248fa71cba6105ff36d054ca70fa7dd49
J K Nanda <jyotikaustuvnanda@MacBook-Pro.local>
2020-11-27 19:52:57 -0800

    Updates

---
+++ precache-manifest.4055c32956b22ab74bad5b47cf1af510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b23a618215f3b5b40e6ddff3f50cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d2855b8bb8cfb6a843a",
+    "url": "/static/css/main.c88684c7.chunk.css"
+  },
+  {
+    "revision": "80116bdca060f6c7f9cb",
+    "url": "/static/js/2.3619dea2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3619dea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2855b8bb8cfb6a843a",
+    "url": "/static/js/main.6043fcb4.chunk.js"
+  },
+  {
+    "revision": "fa67b23c08bf4a6d65d7",
+    "url": "/static/js/runtime-main.6ce803de.js"
+  }
+]);
